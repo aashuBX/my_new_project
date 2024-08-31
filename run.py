b295dc93e750b72dbbfc217acdd65fcd06818c29
aashuBX <zha.aashu0002@gmail.com>
2024-08-31 14:01:23 +0530

    [Aashu] task defination created

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
-    return 'Hello, from all setup done!'
+    return 'Hello, from aashu!'
 
 if __name__ == '__main__':
     app.run()
